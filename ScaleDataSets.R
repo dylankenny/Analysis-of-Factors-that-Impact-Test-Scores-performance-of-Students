f2a5768465b0608886b3ccefa7caf78415040961
dylankenny <128499829+dylankenny@users.noreply.github.com>
2025-03-26 16:28:53 +0000

    Scale datasets
    Scale the datasets so all features contribute equally

---
+++ ScaleDataSets.R
@@ -0,0 +1,6 @@
+
+# Assuming year9_dataset_clean is your dataset after variable removal
+year9_dataset_scaled <- as.data.frame(scale(year9_dataset))
+
+# Assuming year9_dataset_clean is your dataset after variable removal
+year5_dataset_scaled <- as.data.frame(scale(year5_dataset))
